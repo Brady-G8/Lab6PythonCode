6c3ffe5219abec6b2a9b13c0856dfb53e726532f
Josh <Josh@4A-6F-73-68.local>
2024-10-25 17:19:54 -0400

    partner update

--- gitHub_lab.py
+++ gitHub_lab.py
@@ -1,23 +1,28 @@
-#Brady Graham - Updated Final
+# Brady Graham - Updated Final - Josh Updated
 def print_menu():
     print('Menu\n'
           '-----\n'
           '1. Encode\n'
           '2. Decode\n'
           '3. Quit')
-          
+
+
 def encode(message):
-	encoded = ""
-	for digit in message:
-		encoded += str((int(digit) + 3) % 10)
-	
-	return encoded
+    encoded = ""
+    for digit in message:
+        encoded += str((int(digit) + 3) % 10)
+
+    return encoded
+
 
 def decode(encoded_message):
-    print('to be implemented by my partner')
+    decoded = ''
+    for digit in encoded_message:
+        decoded += str((int(digit) - 3) % 10)
+    return decoded
+
 
 def main():
-
     encoded_message = 'hi'
     while True:
 
@@ -37,5 +42,6 @@ def main():
             break
         else:
             print('Input out of range')
+
 
 main()
